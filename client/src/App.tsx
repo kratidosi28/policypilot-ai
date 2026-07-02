29b108418a977f509d992fdd2d030d2289db8a25
kratidosi28 <dosikrati6@gmail.com>
2026-07-02 16:57:41 +0530

    feat: set up React TypeScript frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    //<main> is a semantic HTML container for the main content of the page.
+    <main>
+     <h1>Policy Pilot AI</h1> 
+     <p>Internal HR & IT Support Copilot</p>
+    </main>
+  )
+}
+
+export default App
